acd966ecedc9189834297beffdf6ef963f679aa6
Marco Silva SASC <marcoredesdecomputadores@gmail.com>
2026-05-04 11:53:10 -0300

    Fix: Alteração da função URL que chama a uri padrão do sistema caso não haja nada setado.

--- app/Core/Template.php
+++ app/Core/Template.php
@@ -2,11 +2,14 @@
 
 namespace App\Core;
 
+use App\Utils\ViewClasses;
+
 use Twig\Environment;
 use Twig\Loader\FilesystemLoader as Loader;
 use Twig\Error\LoaderError as Error;
 use Twig\Extension\DebugExtension as Debug;
 use Twig\TwigFunction;
+use Twig\Lexer;
 
 abstract class Template 
 {
@@ -30,11 +33,10 @@ abstract class Template
              * 🔥 Função global url() para usar no Twig
              * Exemplo: {{ url('bsc/store') }}
              */
-            $this->template->addFunction(
-                new TwigFunction('url', function ($path = '') {
-                    return url($path);
-                })
-            );
+            $lexar = new Lexer($this->template, [
+                $this->ViewClasses()
+            ]);
+            $this->template->setLexer($lexar);
 
             $this->template->addFunction(
                 new TwigFunction('form_token_input', function ($key = 'default') {
@@ -54,5 +56,33 @@ abstract class Template
         } catch (Error $th) {
            die("O template {$template}, não existe ou não foi implementado");
         }
+    }
+
+    // Funções auxiliares para o front-end
+    private function ViewClasses(): void
+    {
+        [
+            $this->template->addFunction(new TwigFunction('url', function (string $url) {
+                return ViewClasses::url($url);
+            })),
+            $this->template->addFunction(new TwigFunction('slug', function (string $frase) {
+                return ViewClasses::slug($frase);
+            })),
+            $this->template->addFunction(new TwigFunction('conteTempo', function ($data) {
+                return ViewClasses::countTime($data);
+            })),
+            $this->template->addFunction(new TwigFunction('csrf_field', function () {
+                return ViewClasses::csrf_field();
+            })),
+            $this->template->addFunction(new TwigFunction('saudacao', function () {
+                return ViewClasses::greeting();
+            })),
+            $this->template->addFunction(new TwigFunction('calcDiasAfastado', function ($daybegin, $dayEnd = null, $previewsDayEnd = null) {
+                return ViewClasses::calcDaysAway($daybegin, $dayEnd, $previewsDayEnd);
+            })),
+            $this->template->addFunction(new TwigFunction('resumo', function (string $text, int $limite) {
+                return ViewClasses::resume($text, $limite);
+            }))
+        ];
     }
 }
